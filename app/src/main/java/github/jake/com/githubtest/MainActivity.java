9575efbd03c4cf67a80c3371e89b501d2d312bc1
JakeKyung <cokolavel@gmail.com>
2017-04-28 10:20:39 +0900

    1. Splash Activity Time Setting

--- app/src/main/java/github/jake/com/githubtest/MainActivity.java
+++ app/src/main/java/github/jake/com/githubtest/MainActivity.java
@@ -1,10 +1,9 @@
 package github.jake.com.githubtest;
 
-import android.content.Intent;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,15 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button = (Button)findViewById(R.id.splash);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SplashActivity.class);
-                startActivity(intent);
-            }
-        });
 
     }
 }
--- app/src/main/java/github/jake/com/githubtest/SplashActivity.java
+++ app/src/main/java/github/jake/com/githubtest/SplashActivity.java
@@ -1,13 +1,28 @@
 package github.jake.com.githubtest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends ParentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        getDefaultHandler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                startActivity();
+            }
+        }, 2000);
+    }
+
+
+    private void startActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.abc_fade_in, android.R.anim.fade_out);
+        this.finish();
     }
 }
